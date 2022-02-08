bee88c602865e29984de7a0308a6f00a34cf1e71
Cestarollo Edoardo <edoardo.cestarollo@marconirovereto.it>
2022-02-08 14:56:25 +0000

    prova

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask import Flask, request, Response
+from database.db import initialize_db
+from database.models import Movie
 
 IMAGES_PATH = "/static/img"
 POSTER_FORMAT_NAME = "{}_Poster.jpg"
@@ -21,26 +24,11 @@ def filterMovies(movies):
 app = Flask(__name__)
 CORS(app)
 
-movies = [
-    {
-        "name": "The Shawshank Redemption",
-        "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
-        "director": "Frank Darabont",
-        "director_avatar": "Frank-Darabont_avatar.jpeg",
-        "genres": ["Drama"],
-        "poster": "Shawshank-Redemption_Poster.jpg",
-        "release_date": "September 10, 1994",
-    },
-    {
-       "name": "The Godfather ",
-       "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
-       "director": "Francis Ford Coppola",
-       "director_avatar": "Francis-Ford-Coppola_avatar.jpg",
-       "genres": ["Crime", "Drama"],
-       "poster": "The-Godfather_Poster.jpg",
-       "release_date": "March 14, 1972",
-    }
-]
++app.config['MONGODB_SETTINGS'] = {
++    'host': 'mongodb://localhost/movie-bag'
++}
++
++initialize_db(app)
 
 @app.route("/movies")
 def show_movies():
